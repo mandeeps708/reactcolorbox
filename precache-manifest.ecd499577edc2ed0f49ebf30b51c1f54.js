4749e69a201528f1ebb358243aa47babb7f6737c
Mandeep Singh <mandeeps708@users.noreply.github.com>
2019-03-31 12:45:18 +0530

    Updates

---
+++ precache-manifest.ecd499577edc2ed0f49ebf30b51c1f54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6defba3e758e15106b76",
+    "url": "/reactcolorbox/static/js/runtime~main.a6be6638.js"
+  },
+  {
+    "revision": "06a6b7596205ca439629",
+    "url": "/reactcolorbox/static/js/main.f24d5de8.chunk.js"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/reactcolorbox/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "06a6b7596205ca439629",
+    "url": "/reactcolorbox/static/css/main.4119b81e.chunk.css"
+  },
+  {
+    "revision": "9eeae6e62c9afba2c4108cc635294b71",
+    "url": "/reactcolorbox/index.html"
+  }
+];
